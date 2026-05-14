0c7f76e7970ce069cff6e44c76e63de8b20ce629
NBinte <nuzhatbintei@gmail.com>
2026-05-14 16:49:08 +0600

    Add sample 17

---
+++ switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	a := 1
+	
+	switch a{
+	case 1: 
+		fmt.Println("one")
+	case 2: 
+		fmt.Println("two")
+	default:
+		fmt.Println("default")
+	}
+}
