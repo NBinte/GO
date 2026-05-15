adbe05c560b515b76a041739c1197b4040b2cb9d
NBinte <nuzhatbintei@gmail.com>
2026-05-15 16:02:57 +0600

    Add sample 42

---
+++ sliceCapacity.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	// pointer -> arr[1]
+	// length -> 2
+	// capacity -> 4
+	
+	arr := [5]int{1, 2, 3, 4, 5}
+	sl := arr[1:3]
+	fmt.Println(sl, len(sl), cap(sl))
+}
