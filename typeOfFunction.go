47bcea0410b07c7206e3182777276881862dda68
NBinte <nuzhatbintei@gmail.com>
2026-05-19 00:47:29 +0600

    Add sample 62

---
+++ typeOfFunction.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func callFunc(callable func(int) int) int{
+	return callable(10)
+}
+
+func doubleNumber(num int) int{
+	return 2 * num
+}
+
+func tripleNumber(num int) int{
+	return 3 * num 
+}
+
+func main(){
+	value := callFunc(doubleNumber)
+	fmt.Println(value)
+	fmt.Printf("%T", doubleNumber)
+}
